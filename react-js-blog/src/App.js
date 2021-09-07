77705f33939d5b362426b8e5562ad04873364528
Hasan Iqbal <swapniliqbal@gmail.com>
2021-09-07 03:52:05 -0400

    Initial Styles for the Navbar Component

---
+++ react-js-blog/src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+
+  return (
+    <div className="App">
+
+      {/* Navbar */}
+      <Navbar />
+
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
