0fca2f06219c706f4d945993a20582a9730e0a9f
Hasan Iqbal <swapniliqbal@gmail.com>
2021-09-15 18:10:44 -0400

    Testing Button

--- react-js-blog/src/Home.js
+++ react-js-blog/src/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
+
+    const handleClick = (name) => {
+        console.log("sfsdnjfnsdf" + name)
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            {/* Onclick wrapped in {} because its taking in a dynamic value. Dyn value calls an anon func that calls handleClick. Need to do it this way to pass in params. */}
+            <button onClick = {() => handleClick("all")}>Click</button>
         </div>
      );
 }
